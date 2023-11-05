b5de34a07143d172196acf2c0933e489a68b0f2d
anshjoseph <ranshjosrph@gmail.com>
2023-11-05 11:56:01 +0530

    build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+pip install tensorflow
+pip install pickle
+pip install unicorn['standard']
+pip install python-multipart
+pip install fastapi
+pip install opencv-python
+pip install matplotlib
+pip install scipy
+pip install librosa
